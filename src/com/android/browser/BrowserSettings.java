ff3289d000d6aefc372620e97ff00df9f228a970
Yida Wang <yidaw@codeaurora.org>
2012-01-06 17:16:30 -0500

    webkit: Support for video preloading
    Support for video preloading and runtime media preload
    browser configuration.
    
    for runtime configuration with 3 options:
        - Never
        - Only on WiFi (default)
        - Always
    
    This change also includes logic to determine if the internet
    connection is WiFi when onPageStarted is called in Controller.
    
    Change-Id: I38976ba6c3877c3a8a993b0491af6b26a067e47e

--- src/com/android/browser/BrowserSettings.java
+++ src/com/android/browser/BrowserSettings.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2011 The Android Open Source Project
- * Copyright (c) 2011, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2011, 2012, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -862,5 +862,21 @@ public class BrowserSettings implements OnSharedPreferenceChangeListener,
 
     public String getPreloadEnabled() {
         return mPrefs.getString(PREF_DATA_PRELOAD, getDefaultPreloadSetting());
+    }
+
+    public String getVideoPreloadOnWifiOnlyPreferenceString() {
+        return mContext.getResources().getString(R.string.pref_video_preload_value_wifi_only);
+    }
+
+    public String getVideoPreloadAlwaysPreferenceString() {
+        return mContext.getResources().getString(R.string.pref_video_preload_value_always);
+    }
+
+    public String getDefaultVideoPreloadSetting() {
+        return mContext.getResources().getString(R.string.pref_video_preload_default_value);
+    }
+
+    public String getVideoPreloadEnabled() {
+        return mPrefs.getString(PREF_VIDEO_PRELOAD, getDefaultVideoPreloadSetting());
     }
 }
